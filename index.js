c358bb91c0db8de5856c6a3270e4e3420dd2bbd8
BuildTools <unconfigured@null.spigotmc.org>
2021-02-23 15:53:25 -0600

    Optimize File Structure Fixes #9

--- index.js
+++ index.js
@@ -2,89 +2,89 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 
-
 const PORT=8080;
 http.createServer(function(request, response) {
-    if(request.url=='/'){
-        fs.readFile('./index.html', function (err, html) {
-             if (err) {
-                  throw err
-             }
-             response.writeHeader(200, {"Content-Type": "text/html"});
-             response.write(html);
-             response.end();
-        });
-    } else if(request.url=='/js/main.js'){
-        fs.readFile('./js/main.js', function (err, jsFile) {
-             if (err) {
-                  throw err
-             }
-             response.writeHeader(200, {"Content-Type": "text/javascript"});
-             response.write(jsFile);
-             response.end();
-        });
-    } else if (request.url=='/AsepriteSaves/Block.png'){
-        fs.readFile('./AsepriteSaves/Block.png', function (err, pngFile) {
-            if (err){
-                throw err
-            }
-            response.writeHeader(200, {"Content-Type": "image/png"});
-            response.write(pngFile);
-            response.end();
-        });
-    } else if (request.url=='/AsepriteSaves/Grass.png'){
-        fs.readFile('./AsepriteSaves/Grass.png', function (err, pngFile) {
-            if (err){
-                throw err
-            }
-            response.writeHeader(200, {"Content-Type": "image/png"});
-            response.write(pngFile);
-            response.end();
-        });
-    } else if (request.url=='/favicon.ico'){
-        fs.readFile('./favicon.ico', function (err, icoFile) {
-            if (err){
-                throw err
-            }
-            response.writeHeader(200, {"Content-Type": "image/ico"});
-            response.write(icoFile);
-            response.end();
-        });
-    } else if(request.url=='/js/cube.js'){
-        fs.readFile('./js/cube.js', function (err, jsFile) {
-             if (err) {
-                  throw err
-             }
-             response.writeHeader(200, {"Content-Type": "text/javascript"});
-             response.write(jsFile);
-             response.end();
-        });
-    } else if(request.url=='/js/movement.js'){
-        fs.readFile('./js/movement.js', function (err, jsFile) {
-             if (err) {
-                  throw err
-             }
-             response.writeHeader(200, {"Content-Type": "text/javascript"});
-             response.write(jsFile);
-             response.end();
-        });
-    } else if(request.url=='/js/print.js'){
-        fs.readFile('./js/print.js', function (err, jsFile) {
-             if (err) {
-                  throw err
-             }
-             response.writeHeader(200, {"Content-Type": "text/javascript"});
-             response.write(jsFile);
-             response.end();
-        });
-    } else if(request.url=='/js/World_Generation.js'){
-        fs.readFile('./js/World_Generation.js', function (err, jsFile) {
-             if (err) {
-                  throw err
-             }
-             response.writeHeader(200, {"Content-Type": "text/javascript"});
-             response.write(jsFile);
-             response.end();
-        });
+    switch (request.url) {
+        case '/':
+            fs.readFile('./index.html', function (err, html) {
+                if (err) {
+                     throw err
+                }
+                response.writeHeader(200, {"Content-Type": "text/html"});
+                response.write(html);
+                response.end();
+           });
+        case '/js/main.js':
+            fs.readFile('./js/main.js', function (err, jsFile) {
+                if (err) {
+                     throw err
+                }
+                response.writeHeader(200, {"Content-Type": "text/javascript"});
+                response.write(jsFile);
+                response.end();
+           });
+        case '/AsepriteSaves/Block.png':
+            fs.readFile('./AsepriteSaves/Block.png', function (err, pngFile) {
+                if (err){
+                    throw err
+                }
+                response.writeHeader(200, {"Content-Type": "image/png"});
+                response.write(pngFile);
+                response.end();
+            });
+        case '/AsepriteSaves/Grass.png':
+            fs.readFile('./AsepriteSaves/Grass.png', function (err, pngFile) {
+                if (err){
+                    throw err
+                }
+                response.writeHeader(200, {"Content-Type": "image/png"});
+                response.write(pngFile);
+                response.end();
+            });
+        case '/favicon.ico':
+            fs.readFile('./favicon.ico', function (err, icoFile) {
+                if (err){
+                    throw err
+                }
+                response.writeHeader(200, {"Content-Type": "image/ico"});
+                response.write(icoFile);
+                response.end();
+            });
+        case '/js/cube.js':
+            fs.readFile('./js/cube.js', function (err, jsFile) {
+                if (err) {
+                     throw err
+                }
+                response.writeHeader(200, {"Content-Type": "text/javascript"});
+                response.write(jsFile);
+                response.end();
+           });
+        case '/js/movement.js':
+            fs.readFile('./js/movement.js', function (err, jsFile) {
+                if (err) {
+                     throw err
+                }
+                response.writeHeader(200, {"Content-Type": "text/javascript"});
+                response.write(jsFile);
+                response.end();
+           });
+        case '/js/print.js':
+            fs.readFile('./js/print.js', function (err, jsFile) {
+                if (err) {
+                     throw err
+                }
+                response.writeHeader(200, {"Content-Type": "text/javascript"});
+                response.write(jsFile);
+                response.end();
+           });
+        case '/js/World_Generation.js':
+            fs.readFile('./js/World_Generation.js', function (err, jsFile) {
+                if (err) {
+                     throw err
+                }
+                response.writeHeader(200, {"Content-Type": "text/javascript"});
+                response.write(jsFile);
+                response.end();
+           });
     }
 }).listen(PORT);
